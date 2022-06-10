00d097eb44ae8fe37e0345fbf25deeda1d1b506f
Farida-Hamid <faridaaddala@gmail.com>
2022-06-10 15:36:25 +0300

    Add input tag for new tasks

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import './style.css';
+import Tasks from './tasks-manager.js';
 
-const list = [
-  {
-    description: 'Wake up at 6',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Pray',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Make breakfast',
-    completed: false,
-    index: 3,
-  },
-];
+// const list = [
+//   {
+//     description: 'Wake up at 6',
+//     completed: false,
+//     index: 1,
+//   },
+//   {
+//     description: 'Pray',
+//     completed: false,
+//     index: 2,
+//   },
+//   {
+//     description: 'Make breakfast',
+//     completed: false,
+//     index: 3,
+//   },
+// ];
+
+let list = new Tasks();
 
 const listContainer = document.querySelector('#list');
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.js',
-    mode: 'development',
+    // mode: 'development',
   },
   devServer: {
     static: './dist',
