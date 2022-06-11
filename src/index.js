da0b005cde7778f31b21b76c62741ba4e14a9a8f
Farida-Hamid <faridaaddala@gmail.com>
2022-06-11 11:35:24 +0300

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import './style.css';
 import Tasks from './tasks-manager.js';
 
-// const list = [
-//   {
-//     description: 'Wake up at 6',
-//     completed: false,
-//     index: 1,
-//   },
-//   {
-//     description: 'Pray',
-//     completed: false,
-//     index: 2,
-//   },
-//   {
-//     description: 'Make breakfast',
-//     completed: false,
-//     index: 3,
-//   },
-// ];
-
 let list = new Tasks();
-console.log("list = ", list);
-console.log('loading');
 list.loader();
-console.log('loaded');
 
 const adder = document.getElementById("new-task");
 console.log(adder);
 
 adder.addEventListener("keypress", (e)=> {
-  // e.preventDefault();
-  console.log("here");
-    if (e.key === 'Enter' && adder.value) {
-      console.log("entered");
-      console.log(adder.value);
+
+  if (e.key === 'Enter' && adder.value) {
       const task = {
         description: adder.value,
         completed: false,
@@ -43,14 +19,3 @@ adder.addEventListener("keypress", (e)=> {
       list.display(task);
     }
   });
-
-  console.log("I started");
-  const checkbox = document.querySelector("#done");
-
-checkbox.addEventListener('change', function() {
-if (task.checked) {
-  console.log("Checkbox is checked..");
-} else {
-  console.log("Checkbox is not checked..");
-}
-});
--- src/tasks-manager.js
+++ src/tasks-manager.js
@@ -1,21 +1,13 @@
-import { forEach } from "lodash";
-
 export default class Tasks {
   constructor () {
-  //   this.list = [];
-  //   console.log("after init lis = ", this.list);
     this.list = this.get();
-    console.log("after get lis = ", this.list);
   }
 
   get = () => {
-    // let list;
     const data = localStorage.getItem('memory');
     if (!data) {
-      console.log('yeah I loaded nothing =', data);
       this.list = [];
     } else {
-      console.log('Oh I found =', data);
       this.list = JSON.parse(data);
     }
     return this.list;
@@ -33,11 +25,9 @@ export default class Tasks {
   }
 
   loader = () => {
-    console.log("I want to load", this.list);
     if(this.list.length > 0) {
       this.list.forEach((task) => this.display(task));
     }
-    else{console.log("nothing to load");}
   }
   
   add = (task) => {
