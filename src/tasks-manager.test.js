d44dff727d6a6b834e123039f4fc7d598cee98a5
shaheer ch <shaheer.chry@gmail.com>
2022-06-16 19:04:10 +0500

    Add the test function for the delete function

--- src/tasks-manager.test.js
+++ src/tasks-manager.test.js
@@ -1,13 +1,22 @@
 /* eslint-disable */
 jest.mock('./tasks-manager');
-import { add, deleter } from './tasks-manager.js'
+import { add, deleter } from './tasks-manager.js';
 
 describe('Test Project 1:', () => {
   test('Checks adding:', () => {
     let list = [];
-    expect(add(list, 'I was added')).toStrictEqual([{ description: 'I was added', completed: false, index: 0 }]);
+    expect(add(list, 'I was added')).toStrictEqual([
+      { description: 'I was added', completed: false, index: 0 },
+    ]);
   });
 
-  /// ///// add test to delete function here
-
+  test('check deleting:', () => {
+    let list = [
+      { description: 'I was added', completed: false, index: 0 },
+      { description: 'I was added too', completed: false, index: 1 },
+    ];
+    expect(deleter(list, 0)).toStrictEqual([
+      { description: 'I was added too', completed: false, index: 0 },
+    ]);
+  });
 });
