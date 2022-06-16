2733175ae8ec2e0d005d997dc4a07ce177951f89
Muhammad Shaheer <59507815+ShaheerCH@users.noreply.github.com>
2022-06-16 23:24:26 +0500

    Update tasks-manager.js

--- src/__mocks__/tasks-manager.js
+++ src/__mocks__/tasks-manager.js
@@ -27,8 +27,8 @@ function completed(list, index) {
 }
 
 function deleteAll(list) {
-  for (let i = 0; i < list.length; i += 1) { // eslint-disable-line
-    if (list[i].completed = 'true') {
+  for (let i = 0; i < list.length; i += 1) { 
+    if (list[i].completed = 'true') { // eslint-disable-line
       list.splice(i, 1);
       list[i].index = i;
     }
