80b6f67d12d0889c25f0e4f2c75965651642a42a
Muhammad Shaheer <59507815+ShaheerCH@users.noreply.github.com>
2022-06-17 21:25:52 +0500

    Update tasks-manager.test.js

--- src/tasks-manager.test.js
+++ src/tasks-manager.test.js
@@ -1,4 +1,4 @@
-import {
+ import {
   add,
   completed,
   deleter,
@@ -25,14 +25,9 @@ describe('Test Project 1:', () => {
       { description: 'I was added too', completed: false, index: 0 },
     ]);
   });
-});
 
-describe('Test Project 2:', () => {
   test('check Editing:', () => {
-    const list = [
-      { description: 'I was added', completed: false, index: 0 },
-      { description: 'I was added too', completed: false, index: 1 },
-    ];
+    const list = [{ description: 'I was added', completed: false, index: 0 }];
     expect(edit(list, 0, 'I was edited')).toStrictEqual([
       { description: 'I was edited', completed: false, index: 0 },
     ]);
@@ -40,40 +35,36 @@ describe('Test Project 2:', () => {
 
   test('mark completed:', () => {
     const list = [
-      { description: 'I am to be completed', completed: true, index: 0 },
-      { description: 'I was added', completed: false, index: 1 },
-      { description: 'I was added too', completed: false, index: 2 },
-      { description: 'I am to be completed', completed: false, index: 3 },
-      { description: 'I am to be completed', completed: true, index: 4 },
-      { description: 'I was added too', completed: false, index: 5 },
+      { description: 'I am to be completed', completed: false, index: 0 },
     ];
-    expect(completed(list, 3)).toStrictEqual([
-      { description: 'I am to be completed', completed: true, index: 3 },
+    expect(completed(list, 0)).toStrictEqual([
+      { description: 'I am to be completed', completed: true, index: 0 },
     ]);
   });
 
   test('delete completed:', () => {
     const list = [
-      { description: 'I was added', completed: false, index: 0 },
-      { description: 'I was   too', completed: true, index: 1 },
-      { description: 'I was added', completed: false, index: 2 },
-      { description: 'I   added too', completed: true, index: 3 },
-      { description: 'I was added', completed: false, index: 4 },
-      { description: 'I was added too', completed: true, index: 5 },
-      { description: 'I was a d', completed: false, index: 6 },
-      { description: 'I was ad ed too', completed: true, index: 7 },
-      { description: 'I was ad d', completed: false, index: 8 },
-      { description: 'I was added too', completed: true, index: 9 },
-      { description: 'I was a ed', completed: false, index: 10 },
-      { description: 'I was a d too', completed: true, index: 11 },
+      { description: '1', completed: false, index: 0 },
+      { description: '2', completed: false, index: 1 },
+      { description: '3', completed: false, index: 2 },
+      { description: '4', completed: true, index: 3 },
+      { description: '5', completed: false, index: 4 },
+      { description: '6', completed: true, index: 5 },
+      { description: '7', completed: false, index: 6 },
+      { description: '8', completed: true, index: 7 },
+      { description: '9', completed: false, index: 8 },
+      { description: '10', completed: true, index: 9 },
+      { description: '11', completed: false, index: 10 },
+      { description: '12', completed: true, index: 11 },
     ];
     expect(deleteAll(list)).toStrictEqual([
-      { description: 'I was   too', completed: true, index: 0 },
-      { description: 'I   added too', completed: true, index: 1 },
-      { description: 'I was added too', completed: true, index: 2 },
-      { description: 'I was ad ed too', completed: true, index: 3 },
-      { description: 'I was added too', completed: true, index: 4 },
-      { description: 'I was a d too', completed: true, index: 5 },
+      { description: '1', completed: false, index: 0 },
+      { description: '2', completed: false, index: 1 },
+      { description: '3', completed: false, index: 2 },
+      { description: '5', completed: false, index: 3 },
+      { description: '7', completed: false, index: 4 },
+      { description: '9', completed: false, index: 5 },
+      { description: '11', completed: false, index: 6 },
     ]);
   });
 });
