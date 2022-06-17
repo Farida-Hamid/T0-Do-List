9e56364ae5185a0183cc8245df3fc78976d1f5b4
Farida-Hamid <faridaaddala@gmail.com>
2022-06-17 14:12:30 +0300

    Fix linter error

--- src/__mocks__/tasks-manager.js
+++ src/__mocks__/tasks-manager.js
@@ -28,7 +28,7 @@ function completed(list, index) {
 
 function deleteAll(list) {
   for (let i = 0; i < list.length; i += 1) {
-    if (list[i].completed = 'true') {
+    if (list[i].completed) {
       list.splice(i, 1);
       list[i].index = i;
     }
